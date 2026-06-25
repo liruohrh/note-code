24c200250db17d79498299b47954527489906b8d
liruohrh <2372221537@qq.com>
2026-06-26 02:25:50 +0800

    site(config): Explorer

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,15 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
+import * as ExternalPlugin from "./.quartz/plugins";
+
+ExternalPlugin.Explorer({
+    sortFn: (a, b) => {
+        return a.displayName.localeCompare(b.displayName);
+    }
+});
+
+
+
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
